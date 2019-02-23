3b5add1e7e6e3279dc469b61e5c804863be766dc
mass2010chromium <jcpeng24@gmail.com>
2019-02-23 02:04:27 -0800

    Modified setup instructions, added kivy test code

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+import kivy
+#kivy.require('1.0.6') # replace with your current kivy version !
+
+from kivy.app import App
+from kivy.uix.label import Label
 
 
-import cv2
+class MyApp(App):
 
+    def build(self):
+        return Label(text='Hello world')
+
+
+if __name__ == '__main__':
+    MyApp().run()
